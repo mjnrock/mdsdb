e1ee7e2277c4119e6ba207d13ef5dd9ce7c3a331
Matt Nawrocki <mjnrock@umich.edu>
2020-09-29 21:28:37 -0400

    init

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Builder, { FetchJson } from "./lib/Builder";
 
+//! Thenable
 Builder({
     dog: 3,
     fish: () => Math.random(),
@@ -16,3 +17,23 @@ Builder({
 }).then(obj => {
     console.log(obj);
 });
+
+//! AIIFE
+// (async () => {
+//     const obj = await Builder({
+//         dog: 3,
+//         fish: () => Math.random(),
+//         bird: () => Date.now(),
+//         cat: FetchJson("http://api.open-notify.org/iss-now.json"),
+//         squirrel: {
+//             chip: "a#@$fmd1ji23",
+//             meese: true,
+//             squirrel: {
+//                 chip: "223542352353",
+//                 meese: false,
+//             }
+//         }
+//     });
+
+//     console.log(obj);
+// })();
